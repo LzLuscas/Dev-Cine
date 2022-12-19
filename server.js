c9fcb45997b3f3c544a11c8aa44f815c1f6f187f
LzLuscas <115434062+LzLuscas@users.noreply.github.com>
2022-12-19 11:25:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.port || 3200;
+const port = process.env.PORT || 3200;
 
 server.use(middlewares);
 server.use(router);
